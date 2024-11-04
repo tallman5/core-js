36954ef2a000fc12f195a9962890b0dc02e89a53
tallman5 <tallman5@users.noreply.github.com>
2024-11-04 12:59:17 -0500

    adding date functions

--- test/utilities.test.ts
+++ test/utilities.test.ts
@@ -1,10 +1,15 @@
-import { decodeJWT, isNullOrWhitespace } from "../src/core-js/utilities";
+import { decodeJWT, getLongDate, isNullOrWhitespace } from "../src/core-js/utilities";
 
 describe('Utilites', () => {
     it('token screen name should be decoded', () => {
         const accessToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiam9zZXBoQG1jZ3Vya2luLm5ldCIsIm5iZiI6IjE3MjA3MDIxMjciLCJleHAiOjE3MjMyOTQxMjcsInN1YiI6Impvc2VwaEBtY2d1cmtpbi5uZXQiLCJqdGkiOiJjNjNlOWJmZS0zNjcyLTQyNDYtYjg2OS1lODllNjE0N2FhMGYiLCJTY3JlZW5OYW1lIjoidGFsbG1hbiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluaXN0cmF0b3IiLCJpc3MiOiJodHRwczovL3d3dy5raXh2dS5jb20iLCJhdWQiOiJodHRwczovL3d3dy5raXh2dS5jb20ifQ.sbElWoQ55-5ec_lPSwwOenkdIEDYxV023xnGgjmjiBE`;
         const decodedPayload = decodeJWT(accessToken);
         expect(decodedPayload.ScreenName).toEqual(`tallman`);
+    });
+
+    it('should get long date', () => {
+        const rs = getLongDate();
+        expect(rs).toBeTruthy();
     });
 
     it('string should return false', () => {
